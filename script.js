8b1286374f62a06d2a9bc47716004cb783b17ba2
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-05-20 17:38:20 +0200

    añadida pantalla final de puntuacion,falta el history y el css para acabar la funcionalidad basica

--- script.js
+++ script.js
@@ -13,19 +13,30 @@ jQuery(document).ready(function() {
 	var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
 
+
+	$('#goinit').click(function(){
+		picsdisplayed=1;
+		difficulty=0;
+		score=0;
+		$('#begin').show();
+		alert('!!!!!!!!!');
+	});
+
+
 	$('#myCarousel').hide();
 	$('#map').hide();
+	$('#gameover').hide();
 
 	$('#easy').click(function(){
 		difficulty=1;
 	});
 
 	$('#normal').click(function(){
-		difficulty=2;
+		difficulty=3;
 	});
 
 	$('#hard').click(function(){
-		difficulty=3;
+		difficulty=6;
 	});
 
 
@@ -33,6 +44,7 @@ jQuery(document).ready(function() {
 		if(difficulty===0){
 			alert('Elige una dificultad');
 		}else{
+			$("#myCarousel").data("bs.carousel").options.interval = 6000/difficulty;           
 			$('#begin').hide();
 			$('#myCarousel').show();
 			$('#map').show();
@@ -60,7 +72,6 @@ jQuery(document).ready(function() {
 	                    html+='<img id="carousel'+i+'" src="'+data[i].media.m+'"width="100%">'
 	                html+='</div>'
 	            }
-				$("#myCarousel").data("bs.carousel").options.interval = 3000/difficulty;           
 	            $(".carousel-inner").append(html);
 	        } 
 	    });
@@ -95,16 +106,37 @@ jQuery(document).ready(function() {
 		}).addTo(map);
 
 		function onMapClick(e) {
+			$("#myCarousel").data("bs.carousel").options.interval = 0;           
 		    var marker = L.marker(e.latlng).addTo(map);
 		    marker.bindPopup("<b>Coordenadas</b><br>"+e.latlng).openPopup();
 		    var dist=e.latlng.distanceTo(L.latLng(placecoords[0], placecoords[1]))/1000;
 		    score=dist*picsdisplayed;
-		    alert("clic:"+e.latlng+" \norigen:"+L.latLng(placecoords[0], placecoords[1])+"\ndistancia:"+dist+'km'+'\nsitio:'+placetag+'\npuntuacion:'+score);
-		    
-		}
-		
-		map.on('click', onMapClick);
+		    //alert("clic:"+e.latlng+" \norigen:"+L.latLng(placecoords[0], placecoords[1])+"\ndistancia:"+dist+'km'+'\nsitio:'+placetag+'\npuntuacion:'+score);
+		    showScore(dist);
 
 		}
+
+		function showScore(dist){
+			$('#myCarousel').hide();
+			$('#map').hide();
+			
+			$('#gameover').html('<h1>'+'LUGAR: '+placetag+'<br>'+
+				                'DISTANCIA: '+dist.toFixed(3)+' Km'+'<br>'+
+				                'PUNTUACIÓN: '+score.toFixed(3)+'</h1>'+'<br>'+
+				                '<p><a  id="goinit" class="btn btn-lg btn-success" href="#" role="button">Volver a empezar</a></p>');
+			$('#gameover').show();
+			$('#goinit').click(function(){
+				picsdisplayed=1;
+				difficulty=0;
+				score=0;
+				$('#gameover').html('');
+				$(".carousel-inner").html('');
+				$('#begin').show();
+			});
+		}
+
+		map.on('click', onMapClick);
+
+	}
 
 });
