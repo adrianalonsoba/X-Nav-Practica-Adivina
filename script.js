43d766d1e22c0de246e7819a54d53f9214fddaec
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-05-21 19:45:52 +0200

    cambios menores

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+
 jQuery(document).ready(function() {
+
 
 	var difficulty=0;
 
@@ -56,13 +58,23 @@ jQuery(document).ready(function() {
 		if(difficulty===0||game===''){
 			alert('Elige una dificultad y juego');
 		}else{
-			$(".carousel-inner").empty();
-			$("#myCarousel").data("bs.carousel").options.interval = 6000/difficulty;          
-			$('#begin').hide();
-			$('#gamezone').show();
 			startGame();
 		}
 	});
+
+	function startGame(){
+		$(".carousel-inner").html('');
+		$("#myCarousel").data("bs.carousel").options.interval = 6000/difficulty;          
+		$('#begin').hide();
+		$('#gamezone').show();
+	    $.getJSON("juegos/"+game, function(data) {
+	        var place = data.features[Math.floor(Math.random()*data.features.length)];
+	        placecoords=place.geometry.coordinates;
+	        placetag=place.properties.Name;
+	        showPics(placetag,placecoords);
+	        drawMap();
+	    });
+	}
 
 	function showPics(tag,coords){
 		$.getJSON(flickerAPI,{
@@ -88,69 +100,55 @@ jQuery(document).ready(function() {
 	    });
 	}
 
-	function startGame(){
-	    $.getJSON("juegos/"+game, function(data) {
-	        var place = data.features[Math.floor(Math.random()*data.features.length)];
-	        placecoords=place.geometry.coordinates;
-	        placetag=place.properties.Name;
-	        showPics(placetag,placecoords);
-	        drawMap();
-	    });
-	     
-	}
-
-	//actualiza el numero de fotos mostradas
-	$("#myCarousel").on("slid.bs.carousel",function(){
-        picsdisplayed++;
-
-        //alert(picsdisplayed);
-	});
-
 	function drawMap(){
 		var map = L.map('map').setView([40.2838, -3.8215], 13);
 		
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 		}).addTo(map);
-
-		function onMapClick(e) {         
-		    var dist=e.latlng.distanceTo(L.latLng(placecoords[0], placecoords[1]))/1000;
-		    score=dist*picsdisplayed;
-		    //alert("clic:"+e.latlng+" \norigen:"+L.latLng(placecoords[0], placecoords[1])+"\ndistancia:"+dist+'km'+'\nsitio:'+placetag+'\npuntuacion:'+score);
-		    showScore(dist);
-		}
-
-		function goTo(state){
-			alert('1111111');
-		}
-
-		function saveHistory(){
-			var stateObj={
-				name:game,
-				date:new Date(),
-				score:score,
-				difficulty:difficulty
-			}
-			history.pushState(stateObj,'Adivina',location.href+game+difficulty);
-			var html= '<a id='+stateObj.name+' href="javascript:goTo('+game+')" class="list-group-item his">'+game+'Puntos: '+score+' Fecha:'+stateObj.date+'</a>';
-			$('#history').append(html);
-
-			
-		}
-
-		function showScore(dist){
-			$(".carousel-inner").empty();
-			$("#myCarousel").data("bs.carousel").options.interval = 5000;           
-
-			$('#gamezone').hide();
-			$('#gameover').html('<h1>'+'LUGAR: '+placetag+'<br>'+
-				                'DISTANCIA: '+dist.toFixed(3)+' Km'+'<br>'+
-				                'PUNTUACIÓN: '+score.toFixed(3)+'</h1>');
-			saveHistory();
-			$('#begin').show();
-		}
 		map.on('click', onMapClick);
 
 	}
+
+	function onMapClick(e) {         
+	    var dist=e.latlng.distanceTo(L.latLng(placecoords[0], placecoords[1]))/1000;
+	    score=dist*picsdisplayed;
+	    showScore(dist);
+	}
+
+	function showScore(dist){
+		$("#myCarousel").data("bs.carousel").options.interval = 0; 
+		$(".carousel-inner").html('');          
+		$('#gamezone').hide();
+		$('#gameover').html('<h1>'+'LUGAR: '+placetag+'<br>'+
+			                'DISTANCIA: '+dist.toFixed(3)+' Km'+'<br>'+
+			                'PUNTUACIÓN: '+score.toFixed(3)+'</h1>');
+		saveHistory();
+		$('#begin').show();
+	}	
+
+	function saveHistory(){
+		var stateObj={
+			name:game,
+			date:new Date(),
+			score:score,
+			difficulty:difficulty
+		}
+		history.pushState(stateObj,'Adivina',location.href+game+difficulty);
+		var html= '<a href="javascript:goTo('+game+','+difficulty+')">'+game+'Puntos: '+score+' Fecha:'+stateObj.date+'</a>'+'<br>';
+		$('#history').append(html);	
+	}
+
+	function goTo(game,difficulty){
+		game=game;
+		difficulty=difficulty;
+		startGame();
+	}
+
+	//actualiza el numero de fotos mostradas
+	$("#myCarousel").on("slid.bs.carousel",function(){
+        picsdisplayed++;
+        //alert(picsdisplayed);
+	});
 
 });
